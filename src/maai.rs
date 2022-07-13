4f8e354a50bc1df9f7c75d21e374dec11d98799d
Maccci <max@maxbeier.dev>
2022-07-13 22:04:15 +0200

    fixed accuracy-cost function

--- src/maai.rs
+++ src/maai.rs
@@ -7,6 +7,7 @@ use crate::core::weight::Weight;
 
 pub struct Maai {
     pub layer_count: i32,
+    pub learning_rate: f64,
     layers: Vec<Layer>,
 }
 
@@ -14,13 +15,11 @@ impl Maai {
     pub fn get_accuracy(&self) -> f64 {
         let mut v: f64 = 0.0;
         let mut n: f64 = 0.0;
-        for layer in &self.layers {
-            for height_index in 0..layer.weights.len() {
-                let weight: &Weight = &layer.weights[height_index];
-                n += height_index as f64;
-                v += cost(weight.end_neuron.activation, weight.aim_value);
-            }
-        }
+        let output_layer: &Layer = self.layers.last().unwrap();
+        output_layer.weights.iter().for_each(|weight| {
+            n += weight.index as f64;
+            v += cost(weight.end_neuron.activation, 0.0);
+        });
         v / n
     }
 
@@ -29,6 +28,7 @@ impl Maai {
         let mut maai = Maai {
             layer_count: 4,
             layers: Vec::new(),
+            learning_rate: 2.0,
         };
 
         for i in 0..maai.layer_count {
@@ -69,8 +69,6 @@ impl Maai {
                 weight = Weight {
                     index: maai.layers[layer_index].height as u8,
                     value: rng.gen::<f64>(),
-                    shift_value: rng.gen::<f64>(),
-                    aim_value: rng.gen_range(0.0..1.0),
                     bias: rng.gen::<f64>(),
                     start_neuron: start_neuron,
                     end_neuron: end_neuron,
@@ -103,7 +101,8 @@ fn relu(v: f64) -> f64 {
 }
 
 fn cost(activation: f64, aim_value: f64) -> f64 {
-    (activation - aim_value).sqrt()
+    let result = activation - aim_value.sqrt();
+    result
 }
 
 fn get_cache() -> fs::File {
