7a25b4840e72276ec7eeac4181aa8668bb84b323
Maccci <max@maxbeier.dev>
2022-07-15 17:09:35 +0200

    restruct and added basic documentation

--- src/core/layer.rs
+++ src/core/layer.rs
@@ -1,18 +1,46 @@
+use crate::core::neuron::Neuron;
 use crate::core::weight::Weight;
 
 #[derive(Debug)]
 pub struct Layer {
-    pub index: i32,
-    pub height: i32,
+    pub index: u32,
+    pub height: u32,
     pub weights: Vec<Weight>,
+    pub neurons: Vec<Neuron>,
 }
 
 impl Layer {
-    pub fn new(index_: i32) -> Layer {
-        Layer {
-            index: index_,
-            height: 16,
+    pub fn new(layers: &Vec<Layer>, index: u32, neuron_count: u32, payload: &Vec<f64>) -> Layer {
+        let mut layer: Layer = Layer {
+            index: index,
+            height: neuron_count,
             weights: Vec::new(),
+            neurons: Vec::new(),
+        };
+
+        if index == 0 {
+            let payload: &Vec<f64> = payload;
+            for input_layer_index in payload {
+                let neuron = Neuron {
+                    activation: 0.0,
+                    index: *input_layer_index as u32,
+                    layer_index: index,
+                };
+
+                layer.neurons.push(neuron);
+
+                println!("{:?}", layer.neurons);
+            }
         }
+
+        layer
+    }
+}
+
+fn relu(v: f64) -> f64 {
+    if v >= 0.0 {
+        v
+    } else {
+        0.01 * v
     }
 }
--- src/maai.rs
+++ src/maai.rs
@@ -1,110 +1,59 @@
-use rand::Rng;
 use std::fs;
 
+use crate::core::inspect::inspect;
 use crate::core::layer::Layer;
-use crate::core::neuron::Neuron;
-use crate::core::weight::Weight;
 
 pub struct Maai {
-    pub layer_count: i32,
     pub learning_rate: f64,
     layers: Vec<Layer>,
 }
 
 impl Maai {
+    /// For the maai system you have to specify how much `Layers` and `Neurons [per Layer]` you would like to use.
+    pub fn new(payload: Vec<f64>, layer_count: u32, neuron_count: u32) -> Maai {
+        let mut maai = Maai {
+            layers: Vec::new(),
+            learning_rate: 2.0,
+        };
+
+        for index in 0..layer_count {
+            let layer_instance = Layer::new(&maai.layers, index, neuron_count, &payload);
+            maai.layers.push(layer_instance);
+        }
+
+        maai
+    }
+
+    // GET FUNCTIONS
+
+    /// With `get_accuracy()` you will get a float which determines how accurate the network is by the training data reference.
+    /// As lower the value is, as more accurate is the network.
     pub fn get_accuracy(&self) -> f64 {
+        return self.cost();
+    }
+
+    // TOOLS
+
+    /// [WORK IN PROGRESS] Currently `inpsect()` just outputs the `Layer` and `Neurons [per Layer]` count.
+    pub fn inspect(&self) {
+        inspect(&self.layers);
+    }
+
+    // PRIVATE FUNCTIONS
+
+    fn cost(&self) -> f64 {
         let mut v: f64 = 0.0;
         let mut n: f64 = 0.0;
         let output_layer: &Layer = self.layers.last().unwrap();
         output_layer.weights.iter().for_each(|weight| {
             n += weight.index as f64;
-            v += cost(weight.end_neuron.activation, 0.0);
+            v += (weight.end_neuron.activation - 0.0).sqrt();
         });
         v / n
     }
-
-    pub fn new(input: Vec<f64>) -> Maai {
-        let mut rng = rand::thread_rng();
-        let mut maai = Maai {
-            layer_count: 4,
-            layers: Vec::new(),
-            learning_rate: 2.0,
-        };
-
-        for i in 0..maai.layer_count {
-            let layer = Layer::new(i);
-            maai.layers.push(layer);
-        }
-
-        for layer_index in 0..maai.layers.len() {
-            for neuron_index in 0..maai.layers[layer_index].height {
-                let start_neuron: Neuron;
-                let end_neuron: Neuron;
-                let mut weight: Weight;
-
-                if layer_index == 0 {
-                    if neuron_index < input.len() as i32 {
-                        start_neuron = Neuron {
-                            activation: input[(neuron_index as usize)],
-                            index: neuron_index,
-                            layer_index: maai.layers[layer_index].index,
-                        }
-                    } else {
-                        continue;
-                    }
-                } else {
-                    start_neuron = Neuron {
-                        activation: rng.gen::<f64>(),
-                        index: neuron_index,
-                        layer_index: maai.layers[layer_index].index,
-                    };
-                }
-
-                end_neuron = Neuron {
-                    activation: rng.gen::<f64>(),
-                    index: neuron_index,
-                    layer_index: maai.layers[layer_index].index + 1,
-                };
-
-                weight = Weight {
-                    index: maai.layers[layer_index].height as u8,
-                    value: rng.gen::<f64>(),
-                    bias: rng.gen::<f64>(),
-                    start_neuron: start_neuron,
-                    end_neuron: end_neuron,
-                    layer_index: maai.layers[layer_index].index,
-                };
-
-                let mut sum: f64 = 0.0;
-                for weight in &maai.layers[layer_index].weights {
-                    sum += weight.value * weight.start_neuron.activation;
-                }
-
-                weight.end_neuron.activation += relu(sum - weight.bias);
-                maai.layers[layer_index].weights.push(weight);
-            }
-        }
-        maai
-    }
-
-    pub fn run(&self) {
-        println!("{:?}", self.get_accuracy());
-    }
 }
 
-fn relu(v: f64) -> f64 {
-    if v >= 0.0 {
-        v
-    } else {
-        0.01 * v
-    }
-}
-
-fn cost(activation: f64, aim_value: f64) -> f64 {
-    let result = activation - aim_value.sqrt();
-    result
-}
-
+#[allow(dead_code)]
 fn get_cache() -> fs::File {
     let file: fs::File;
     if fs::metadata("../cache/").is_err() {
